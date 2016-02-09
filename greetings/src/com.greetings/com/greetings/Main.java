a99e7564ba96a1ce417af5980f739fb98fa1083e
Clement Trung <clementtrung@hotmail.com>
2016-02-09 22:43:43 +0100

    Greetings example

---
+++ greetings/src/com.greetings/com/greetings/Main.java
@@ -0,0 +1,10 @@
+package com.greetings;
+
+/**
+ * Jigsaw module examples : <a href="http://openjdk.java.net/projects/jigsaw/quick-start#greetings">Greetings</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Greetings!");
+    }
+}
